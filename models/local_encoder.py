86c8afdfd1d7110700580bc907b10b70858e14e7
Conner <cjklusk@gmail.com>
2024-05-29 16:37:12 -0700

    initial

--- models/local_encoder.py
+++ models/local_encoder.py
@@ -61,57 +61,35 @@ class LocalEncoder(nn.Module):
         )
 
     def forward(self, data: TemporalData) -> torch.Tensor:
-        for t in range(self.historical_steps):
-            data[f"edge_index_{t}"], _ = subgraph(
-                subset=~data["padding_mask"][:, t], edge_index=data.edge_index
-            )
-            data[f"edge_attr_{t}"] = (
-                data["positions"][data[f"edge_index_{t}"][0], t]
-                - data["positions"][data[f"edge_index_{t}"][1], t]
-            )
-        if self.parallel:
-            snapshots = [None] * self.historical_steps
+        if data.edge_index.shape > 0:
             for t in range(self.historical_steps):
-                edge_index, edge_attr = self.drop_edge(
-                    data[f"edge_index_{t}"], data[f"edge_attr_{t}"]
+                data[f"edge_index_{t}"], _ = subgraph(
+                    subset=~data["padding_mask"][:, t], edge_index=data.edge_index
                 )
-                snapshots[t] = Data(
-                    x=data.x[:, t],
-                    edge_index=edge_index,
-                    edge_attr=edge_attr,
-                    num_nodes=data.num_nodes,
+                data[f"edge_attr_{t}"] = (
+                    data["positions"][data[f"edge_index_{t}"][0], t]
+                    - data["positions"][data[f"edge_index_{t}"][1], t]
                 )
-            batch = Batch.from_data_list(snapshots)
-            out = self.aa_encoder(
-                x=batch.x,
-                t=None,
-                edge_index=batch.edge_index,
-                edge_attr=batch.edge_attr,
-                bos_mask=data["bos_mask"],
-                rotate_mat=data["rotate_mat"],
+
+                out = [None] * self.historical_steps
+                for t in range(self.historical_steps):
+                    edge_index, edge_attr = self.drop_edge(
+                        data[f"edge_index_{t}"], data[f"edge_attr_{t}"]
+                    )
+                    out[t] = self.aa_encoder(
+                        x=data.x[:, t],
+                        t=t,
+                        edge_index=edge_index,
+                        edge_attr=edge_attr,
+                        bos_mask=data["bos_mask"][:, t],
+                        rotate_mat=data["rotate_mat"],
+                    )
+                out = torch.stack(out)  # [T, N, D]
+            out = self.temporal_encoder(
+                x=out, padding_mask=data["padding_mask"][:, : self.historical_steps]
             )
-            out = out.view(
-                self.historical_steps, out.shape[0] // self.historical_steps, -1
-            )
-        else:
-            out = [None] * self.historical_steps
-            for t in range(self.historical_steps):
-                edge_index, edge_attr = self.drop_edge(
-                    data[f"edge_index_{t}"], data[f"edge_attr_{t}"]
-                )
-                out[t] = self.aa_encoder(
-                    x=data.x[:, t],
-                    t=t,
-                    edge_index=edge_index,
-                    edge_attr=edge_attr,
-                    bos_mask=data["bos_mask"][:, t],
-                    rotate_mat=data["rotate_mat"],
-                )
-            out = torch.stack(out)  # [T, N, D]
-        out = self.temporal_encoder(
-            x=out, padding_mask=data["padding_mask"][:, : self.historical_steps]
-        )
-        return out
+            return out
+        
 
 
 class AAEncoder(MessagePassing):
